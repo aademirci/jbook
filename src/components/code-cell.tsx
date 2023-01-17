e995bf08229ababec9aeaca1794f0d4c23c5aeef
Adnan Alper Demirci <aalperdemirci@gmail.com>
2023-01-17 23:46:55 +0100

    Draggable resizing components

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,24 +1,32 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import CodeEditor from './code-editor'
 import Preview from './preview'
 import bundle from '../bundler'
+import Resizable from "./resizable"
 
 const CodeCell = () => {
     const [code, setCode] = useState('')
     const [input, setInput] = useState('')
 
-    const onClick = async () => {
-        const output = await bundle(input)
-        setCode(output)
-    }
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            const output = await bundle(input)
+            setCode(output)
+        }, 1000)
 
-    return <div>
-        <CodeEditor initialValue='//code here' onChange={(value) => setInput(value)} />
-        <div>
-            <button onClick={onClick}>Submit</button>
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [input])
+
+    return <Resizable direction="vertical">
+        <div style={{ height: '100%', display: 'flex', flexDirection: 'row'}}>
+            <Resizable direction="horizontal">
+                <CodeEditor initialValue='//code here' onChange={(value) => setInput(value)} />
+            </Resizable>
+            <Preview code={code} />
         </div>
-        <Preview code={code} />
-    </div>
+    </Resizable>
 }
 
 export default CodeCell
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css'
 import { useEffect, useRef } from "react"
 
 interface PreviewProps {
@@ -6,7 +7,11 @@ interface PreviewProps {
 
 const html = `
     <html>
-        <head></head>
+        <head>
+            <style>
+                html { background-color: white; }
+            </style>
+        </head>
         <body>
             <div id="root"></div>
             <script>
@@ -29,10 +34,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
     useEffect(() => {
         iframe.current.srcdoc = html
-        iframe.current.contentWindow.postMessage(code, '*')
+        setTimeout(() => {
+            iframe.current.contentWindow.postMessage(code, '*')
+        }, 50)
     }, [code])
     
-    return <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='preview' />
+    return <div className="preview-wrapper">
+            <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='preview' />
+        </div>
 }
 
 export default Preview
