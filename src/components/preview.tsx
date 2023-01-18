59a1370e33f6bccc28202d94c80cbb17ba2fe33f
Adnan Alper Demirci <aalperdemirci@gmail.com>
2023-01-18 22:29:15 +0100

    Error handling

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react"
 
 interface PreviewProps {
     code: string
+    err: string
 }
 
 const html = `
@@ -15,13 +16,22 @@ const html = `
         <body>
             <div id="root"></div>
             <script>
+                const handleError = (err) => {
+                    const root = document.querySelector('#root')
+                    root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>'
+                    throw err
+                }
+
+                window.addEventListener('error', (event) => {
+                    event.preventDefault()
+                    handleError(event.error)
+                })
+
                 window.addEventListener('message', (event) => {
                     try {
                         eval(event.data)
                     } catch (err) {
-                        const root = document.querySelector('#root')
-                        root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>'
-                        throw err
+                        handleError(err)
                     }
                 }, false)
             </script>
@@ -29,7 +39,7 @@ const html = `
     </html>
 `
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
     const iframe = useRef<any>()
 
     useEffect(() => {
@@ -41,6 +51,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
     
     return <div className="preview-wrapper">
             <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='preview' />
+            {err && <div className='preview-error'>{err}</div>}
         </div>
 }
 
