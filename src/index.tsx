cb3776341c42195d2f4079f358395736c213a1e5
Adnan Alper Demirci <aalperdemirci@gmail.com>
2023-02-10 21:04:50 +0100

    Design patterns for Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from "react-dom"
+import { Provider } from 'react-redux'
+import { store } from './state'
 import TextEditor from './components/text-editor'
 
 const App = () => {
-    return <div>
+    return <Provider store={store}>
+        <div>
         <TextEditor />
     </div>
+    </Provider>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
